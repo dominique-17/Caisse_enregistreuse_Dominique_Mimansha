d68b09b7587b777edeccb16b6bbe66887be7063f
dominique-17 <dtangaevengue@gmail.com>
2021-03-24 08:43:52 +0400

    l'utilisateur doit d'entrer une valeur qu'il doit payer

--- Script/caisse.c
+++ Script/caisse.c
@@ -1,10 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-double main()
+double aleatoire()
 
 {
-	double nombre = rand();
-	printf("%f\n", nombre );
-	return nombre;
+    double nombre = rand();
+   //printf("%.2f\n", nombre);
+   return nombre ;
 }
+
+int main(int argc, char const *argv[])
+{
+	double rand = aleatoire();
+	double num = 0.0;
+
+		printf("veuillez entrez votre valeur : ");
+		scanf("%lf" ,&num);
+
+	while (num < rand){
+		printf("valeur inferieure\n");
+		printf("veuillez entrez votre valeur : ");
+		scanf("%lf" ,&num);
+	}
+	return 0;
+}
+
+
